f8f51bad7ac6cbcb96d22b788ce8db7bd4887a8a
Denis Kutnár <deni2346@stud.kea.dk>
2019-04-05 13:11:48 +0200

    FormatToolbar

---
+++ my-app/src/components/BoldMark.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+const BoldMark = props => (
+  <strong>
+    {props.children}
+  </strong>
+);
+
+export default BoldMark;
--- my-app/src/components/TextEditor.js
+++ my-app/src/components/TextEditor.js
@@ -1,8 +1,8 @@
 
-import React, {Component} from 'react'
-import {Editor} from 'slate-react'
-import {Value} from 'slate'
-
+import React, { Component } from 'react'
+import { Editor } from 'slate-react'
+import { Value } from 'slate'
+import { BoldMark, ItalicMark } from './index';
 
 // Create our initial value...
 const initialValue = Value.fromJSON({
@@ -27,21 +27,70 @@ const initialValue = Value.fromJSON({
 })
 
 
-export default class TextEditor extends Component{
+export default class TextEditor extends Component {
 
-  state = {
+
+	state = {
 		value: initialValue,
 	}
 
-  onChange = ({value}) =>{
-    this.setState({value})
-  }
+	// On change, update the app's React state with the new editor value.
+	onChange = ({ value }) => {
+		this.setState({ value })
+	}
 
+	onKeyDown = (e, change) => {
 
-render(){
-  return (
-    <Editor value={this.state.value} onChange={this.onChange} />
-  )
-}
+		/*
+			we want all our commands to start with the user pressing ctrl,
+			if they don't--we cancel the action.
+		*/
 
+		if (!e.ctrlKey) { return }
+		e.preventDefault()
+
+		/* Decide what to do based on the key code... */
+		switch (e.key) {
+			/* When "b" is pressed, add a "bold" mark to the text. */
+			case 'b': {
+				change.toggleMark('bold')
+				return true
+			}
+			case 'i': {
+				change.toggleMark('italic')
+				return true
+			}
+
+			default: {
+				return;
+			}
+
+		}
+	}
+
+	renderMark = props => {
+		switch (props.mark.type) {
+			case 'bold':
+				return <BoldMark {...props} />
+
+			case 'italic':
+				return <ItalicMark {...props} />
+
+			default: {
+				return;
+			}
+		}
+	}
+
+	render() {
+		return (
+			<Editor
+				value={this.state.value}
+				onChange={this.onChange}
+				onKeyDown={this.onKeyDown}
+				renderMark={this.renderMark}
+
+			/>
+		)
+	}
 }
