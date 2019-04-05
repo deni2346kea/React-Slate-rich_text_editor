a693dbab6786ad5d05a4f6cf6dab3e35007826b6
Denis Kutnár <deni2346@stud.kea.dk>
2019-04-05 12:15:43 +0200

    My first paragraph

--- my-app/src/components/TextEditor.js
+++ my-app/src/components/TextEditor.js
@@ -3,15 +3,40 @@ import React, {Component} from 'react'
 import {Editor} from 'slate-react'
 import {Value} from 'slate'
 
+
+// Create our initial value...
+const initialValue = Value.fromJSON({
+	document: {
+		nodes: [
+			{
+				object: 'block',
+				type: 'paragraph',
+				nodes: [
+					{
+						object: 'text',
+						leaves: [
+							{
+								text: 'My first paragraph!',
+							},
+						],
+					},
+				],
+			},
+		],
+	},
+})
+
+
 export default class TextEditor extends Component{
 
   state = {
-    value: '',
-  }
+		value: initialValue,
+	}
 
   onChange = ({value}) =>{
     this.setState({value})
   }
+
 
 render(){
   return (
